01d3e98e55e98f7e14d40be0ed905dfb00c1a4eb
Jonas Engström <j.e.engstrom@gmail.com>
2024-11-18 15:18:05 +0900

    Added minimum monthly payment property.

--- src/loan/mortgage.py
+++ src/loan/mortgage.py
@@ -13,6 +13,22 @@ class Mortgage:
         principal: Amount borrowed.
     """
 
+    # The following dictionaries contain cutoff values for minmum yearly
+    # percentages of the principal to be paid, according to Finansinspektionen.
+    # The keys represent the cutoff value and the values represent the minimum
+    # percentage. E.g. a _loan_to_value_cuttoffs of {0.7: 0.02, 0.5: 0.1.}
+    # means that 2% yearly has to be paid if the loan-to-value ratio is over
+    # 70% and 1% yearly if it is over 50%.
+    _loan_to_value_cutoffs = {0.7: 0.02, 0.5: 0.01}
+    _debt_ratio_cutoffs = {4.5: 0.01}
+
+    @classmethod
+    def _check_cutoff(cls, cutoff_dict: dict, cutoff_value: float) -> float:
+        """Return max dict value where cutoff_value is larger than dict key."""
+        return max(
+            [value if cutoff_value > key else 0 for key, value in cutoff_dict.items()]
+        )
+
     def __init__(
         self, asset_value: float, household_gross_income: float, principal: float
     ) -> None:
@@ -37,3 +53,17 @@ class Mortgage:
     def debt_ratio(self) -> float:
         """Ratio of principal to household gross income."""
         return self.principal / self.household_gross_income
+
+    @property
+    def minimum_monthly_payment(self) -> float:
+        """Return the minimum legal monthly payment amount."""
+        yearly_minimum_percentage = max(
+            type(self)._check_cutoff(
+                type(self)._loan_to_value_cutoffs, self.loan_to_value_ratio
+            ),
+            type(self)._check_cutoff(type(self)._debt_ratio_cutoffs, self.debt_ratio),
+        )
+        minimum_monthly_payment = (
+            self.initial_principal * yearly_minimum_percentage / 12
+        )
+        return minimum_monthly_payment
--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -27,3 +27,35 @@ class TestMortgage(unittest.TestCase):
     def test_debt_ratio(self) -> None:
         """Check that debt ratio calculates correctly."""
         self.assertEqual(self.checker.debt_ratio, 2.5)
+
+    def test__check_cutoff(self) -> None:
+        """Check that check_cutoff returns expected values."""
+        self.assertEqual(
+            self.checker._check_cutoff(type(self.checker)._loan_to_value_cutoffs, 0.4),
+            0,
+        )
+        self.assertEqual(
+            self.checker._check_cutoff(type(self.checker)._loan_to_value_cutoffs, 0.6),
+            0.01,
+        )
+        self.assertEqual(
+            self.checker._check_cutoff(type(self.checker)._loan_to_value_cutoffs, 0.8),
+            0.02,
+        )
+        self.assertEqual(
+            self.checker._check_cutoff(type(self.checker)._debt_ratio_cutoffs, 1), 0
+        )
+        self.assertEqual(
+            self.checker._check_cutoff(type(self.checker)._debt_ratio_cutoffs, 5), 0.01
+        )
+
+    def test_minimum_monthly_payment(self) -> None:
+        """Check that minimum_monthly_payment returns expected values."""
+        self.assertEqual(self.checker.minimum_monthly_payment, 0)
+        self.checker.household_gross_income = 10
+        self.assertEqual(self.checker.minimum_monthly_payment, 5e6 * 0.01 / 12)
+        self.checker.household_gross_income = 2e6
+        self.checker.principal = 6e6
+        self.assertEqual(self.checker.minimum_monthly_payment, 5e6 * 0.01 / 12)
+        self.checker.principal = 8e6
+        self.assertEqual(self.checker.minimum_monthly_payment, 5e6 * 0.02 / 12)
