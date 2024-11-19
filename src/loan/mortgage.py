a8234a9e80b0c69e8ec9729000fd98a3ed41ef2d
Jonas Engström <j.e.engstrom@gmail.com>
2024-11-19 11:28:10 +0900

    Added risk cost calculations and unit tests.

--- src/loan/mortgage.py
+++ src/loan/mortgage.py
@@ -1,3 +1,6 @@
+from datetime import datetime, date
+
+
 class Mortgage:
     """Analyze costs of a Swedish house loan.
 
@@ -9,6 +12,7 @@ class Mortgage:
     Attributes:
         asset_value: Value of the asset mortgaged.
         household_gross_income: Household income before tax.
+        index_fund_value: Value of index fund.
         initial_principal: Original amount borrowed.
         principal: Amount borrowed.
     """
@@ -21,28 +25,108 @@ class Mortgage:
     # 70% and 1% yearly if it is over 50%.
     _loan_to_value_cutoffs = {0.7: 0.02, 0.5: 0.01}
     _debt_ratio_cutoffs = {4.5: 0.01}
+    _risk_cost_per_million_by_age_cutoffs = {
+        20: 0.7,
+        25: 0.72,
+        30: 0.56,
+        35: 0.72,
+        40: 1.05,
+        45: 1.73,
+        50: 2.94,
+        55: 4.66,
+        60: 7.42,
+        65: 13.30,
+        70: 21.1,
+        75: 35.56,
+        80: 63.94,
+        85: 114.4,
+        90: 196.42,
+    }
 
     @classmethod
     def _check_cutoff(cls, cutoff_dict: dict, cutoff_value: float) -> float:
-        """Return max dict value where cutoff_value is larger than dict key."""
+        """Return max dict value where cutoff_value is larger than dict key.
+
+        Args:
+            cutoff_dict: A dict formatted as cutoff values to check: values to
+            return.
+            cutoff_value: The cutoff value to check agains the cutoff dict.
+
+        Returns:
+            The dict value corresponding to cutoff_value.
+        """
         return max(
             [value if cutoff_value > key else 0 for key, value in cutoff_dict.items()]
         )
 
+    @classmethod
+    def _convert_date_to_int(cls, date_to_convert: date) -> int:
+        """Converts a date to an integer, formatted as MMDD.
+
+        Args:
+            date_to_convert: A datetime object.
+
+        Returns:
+            An integer, formatted as MMDD.
+        """
+        return int(date_to_convert.strftime("%m%d"))
+
+    @classmethod
+    def _first_date(cls, date_one: date, date_two: date) -> int:
+        """Check which date comes first in the year, disregarding the year.
+
+        Args:
+            date_one: A datetime object.
+            date_two: A datetime object.
+
+        Returns:
+            The index of the first date in the year. I.e. 0 for date_one or 1
+            for date_two.
+        """
+        date_list = [
+            cls._convert_date_to_int(date_one),
+            cls._convert_date_to_int(date_two),
+        ]
+        first_date_index = date_list.index(min(date_list))
+        return first_date_index
+
     def __init__(
-        self, asset_value: float, household_gross_income: float, principal: float
+        self,
+        asset_value: float,
+        birth_date: str,
+        household_gross_income: float,
+        principal: float,
     ) -> None:
         """Initialize Mortgage instance.
 
         Args:
             asset_value: Value of the asset mortgaged.
+            birth_date: Birth date of index fund owner.
             household_gross_income: Household income before tax.
             principal: Amount borrowed.
         """
         self.asset_value = asset_value
+        self.birth_date = birth_date
+        self._current_date = datetime.now()
         self.household_gross_income = household_gross_income
+        self.index_fund_value = 0
         self.initial_principal = principal
         self.principal = principal
+
+    @property
+    def birth_date(self) -> date:
+        """Return birth date."""
+        return self._birth_date
+
+    @birth_date.setter
+    def birth_date(self, value: str) -> None:
+        """Set birth date from an ISO format string."""
+        self._birth_date = date.fromisoformat(value)
+
+    @property
+    def current_date(self) -> date:
+        """Current date, incremented for future calculations."""
+        return self._current_date
 
     @property
     def loan_to_value_ratio(self) -> float:
@@ -67,3 +151,30 @@ class Mortgage:
             self.initial_principal * yearly_minimum_percentage / 12
         )
         return minimum_monthly_payment
+
+    @property
+    def age(self) -> int:
+        """Return age of index fund owner."""
+        year_delta = self.current_date.year - self.birth_date.year
+        has_not_had_birthday = type(self)._first_date(
+            self.birth_date, self.current_date
+        )
+        return year_delta - has_not_had_birthday
+
+    @property
+    def rounded_age(self) -> int:
+        """Age rounded to closest five between 20 and 150."""
+        match self.age:
+            case self.age if self.age < 20:
+                return 20
+            case self.age if self.age > 90:
+                return 90
+            case _:
+                return 5 * round(self.age / 5)
+
+    @property
+    def risk_cost(self) -> float:
+        risk_cost_per_million = self._risk_cost_per_million_by_age_cutoffs[
+            self.rounded_age
+        ]
+        return self.index_fund_value * risk_cost_per_million / 1e6
--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -2,6 +2,8 @@ import unittest
 
 from src.loan import mortgage
 
+from datetime import datetime, date
+
 
 class TestMortgage(unittest.TestCase):
     """Test case for Mortgage class."""
@@ -10,12 +12,19 @@ class TestMortgage(unittest.TestCase):
         """Set up test case for Mortgage class."""
         super().__init__(method_name)
         self.checker = mortgage.Mortgage(
-            asset_value=10e6, household_gross_income=2e6, principal=5e6
+            asset_value=10e6,
+            birth_date="2006-09-02",
+            household_gross_income=2e6,
+            principal=5e6,
         )
 
-    def test__init__(self) -> None:
+    def test___init__(self) -> None:
         """Check that __init__ exhibits expected behavior."""
         self.assertEqual(self.checker.asset_value, 10e6)
+        self.assertIsInstance(self.checker.birth_date, date)
+        self.assertEqual(self.checker.birth_date.year, 2006)
+        self.assertEqual(self.checker.birth_date.month, 9)
+        self.assertEqual(self.checker.birth_date.day, 2)
         self.assertEqual(self.checker.household_gross_income, 2e6)
         self.assertEqual(self.checker.initial_principal, 5e6)
         self.assertEqual(self.checker.principal, 5e6)
@@ -49,6 +58,13 @@ class TestMortgage(unittest.TestCase):
             self.checker._check_cutoff(type(self.checker)._debt_ratio_cutoffs, 5), 0.01
         )
 
+    def test__convert_date_to_int(self) -> None:
+        """Check that _convert_date_to_int returns expected value."""
+        self.assertEqual(
+            type(self.checker)._convert_date_to_int(date.fromisoformat("1998-03-05")),
+            305,
+        )
+
     def test_minimum_monthly_payment(self) -> None:
         """Check that minimum_monthly_payment returns expected values."""
         self.assertEqual(self.checker.minimum_monthly_payment, 0)
@@ -59,3 +75,86 @@ class TestMortgage(unittest.TestCase):
         self.assertEqual(self.checker.minimum_monthly_payment, 5e6 * 0.01 / 12)
         self.checker.principal = 8e6
         self.assertEqual(self.checker.minimum_monthly_payment, 5e6 * 0.02 / 12)
+
+    def test_set_birth_date(self) -> None:
+        """Check that incorrect values raise error when setting birth date."""
+        with self.assertRaises(ValueError):
+            self.checker.birth_date = "not a date"
+
+    def test_current_date(self) -> None:
+        """Check that current date is set correctly."""
+        self.assertEqual(self.checker.current_date.year, datetime.now().year)
+        self.assertEqual(self.checker.current_date.month, datetime.now().month)
+        self.assertEqual(self.checker.current_date.day, datetime.now().day)
+
+    def test__first_date(self) -> None:
+        """Check that _first_date returns expected value."""
+        self.assertEqual(
+            type(self.checker)._first_date(
+                date.fromisoformat("1998-03-01"), date.fromisoformat("1986-07-08")
+            ),
+            0,
+        )
+
+    def test_age_has_had_birthday(self) -> None:
+        """Tests whether age returns correct value after birthday."""
+        self.checker._current_date = date.fromisoformat("2007-09-03")
+        self.assertEqual(self.checker.age, 1)
+
+    def test_age_has_not_had_birthday(self) -> None:
+        """Tests whether age returns correct value before birthday."""
+        self.checker._current_date = date.fromisoformat("2024-09-01")
+        self.assertEqual(self.checker.age, 17)
+
+    def test_risk_cost(self) -> None:
+        """Check that risk cost calculations are correct."""
+        self.checker.index_fund_value = 0.5e6
+        for (
+            age,
+            cost_per_million,
+        ) in self.checker._risk_cost_per_million_by_age_cutoffs.items():
+            self.checker._birth_date = date(
+                year=self.checker.current_date.year - age, month=1, day=1
+            )
+            self.assertEqual(self.checker.risk_cost, cost_per_million / 2)
+
+    def test_rounded_age(self) -> None:
+        """Check that rounded_age rounds to the closest 5 between 20 and 90."""
+        ages_to_round = {2: 20, 150: 90}
+        ages_to_round.update(
+            {
+                i[0]: i[1]
+                for i in zip(
+                    [i for i in range(19, 90, 5)], [i for i in range(20, 90, 5)]
+                )
+            }
+        )
+        ages_to_round.update(
+            {
+                i[0]: i[1]
+                for i in zip(
+                    [i for i in range(21, 90, 5)], [i for i in range(20, 90, 5)]
+                )
+            }
+        )
+        ages_to_round.update(
+            {
+                i[0]: i[1]
+                for i in zip(
+                    [i for i in range(17, 90, 5)], [i for i in range(20, 90, 5)]
+                )
+            }
+        )
+        ages_to_round.update(
+            {
+                i[0]: i[1]
+                for i in zip(
+                    [i for i in range(22, 90, 5)], [i for i in range(20, 90, 5)]
+                )
+            }
+        )
+        for actual_age, rounded_age in ages_to_round.items():
+            self.checker._birth_date = date(
+                year=self.checker.current_date.year - actual_age, month=1, day=1
+            )
+            self.assertEqual(self.checker.rounded_age, rounded_age)
