1b6f576784b47d219f134fc99f074972bc375af9
Jonas Engström <j.e.engstrom@gmail.com>
2024-11-20 18:24:07 +0900

    Added a function to download policy rate.

--- src/loan/download.py
+++ src/loan/download.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 
 from io import StringIO
+from datetime import datetime
 
 
 def government_borrowing_rate():
@@ -50,12 +51,26 @@ def consumer_price_index():
         return_df = (
             pd.read_csv(data_string)
             .drop(["ContentsCode"], axis=1)
-            .rename(columns={"Tid": "Datum", "TAB5737": "Konsumentprisindex"})
+            .rename(columns={"Tid": "date", "TAB5737": "consumer_price_index"})
         )
-        return_df["Datum"] = return_df["Datum"].str.replace(
+        return_df["date"] = return_df["date"].str.replace(
             r"M(\d+)", r"-\1-01", regex=True
         )
-        return_df["Datum"] = pd.to_datetime(return_df["Datum"])
+        return_df["date"] = pd.to_datetime(return_df["date"])
+        return return_df
+    else:
+        raise ConnectionError(
+            f"The connection to {url} got a status code of {request.status_code}, instead of status code 200."
+        )
+
+
+def policy_rate():
+    url = f"https://api.riksbank.se/swea/v1/Observations/SECBREPOEFF/1994-06-01/{datetime.now().strftime('%Y-%m-%d')}"
+    request = requests.get(url)
+    if request.status_code == 200:
+        string_data = StringIO(request.text)
+        return_df = pd.read_json(string_data)
+        return_df = return_df.rename(columns={"value": "policy_rate"})
         return return_df
     else:
         raise ConnectionError(
--- tests/test_download.py
+++ tests/test_download.py
@@ -14,7 +14,7 @@ class MockResponse:
         self.status_code = status_code
 
 
-def mocked_request_get_404(*args, **kwargs):
+def mocked_request_404(*args, **kwargs):
     """Mock 404 status code."""
     return MockResponse("foo", 404)
 
@@ -25,9 +25,15 @@ def mocked_requests_get_200_gbr(*args, **kwargs):
     return MockResponse(text, 200)
 
 
-def mocked_requests_get_200_cpi(*args, **kwargs):
+def mocked_requests_post_200_cpi(*args, **kwargs):
     """Mock consumer price index CSV."""
     text = '"Tid","ContentsCode","TAB5737"\r\n"1980M01","000004VU",95.30\r\n"1980M02","000004VU",96.80\r\n"1980M03","000004VU",97.20\r\n'
+    return MockResponse(text, 200)
+
+
+def mocked_requests_get_200_pr(*args, **kwargs):
+    """Mock policy rate CSV."""
+    text = '[{"date":"1994-06-01","value":6.95},{"date":"1994-06-02","value":6.95},{"date":"1994-06-03","value":6.95}]'
     return MockResponse(text, 200)
 
 
@@ -37,7 +43,7 @@ class TestDownload(unittest.TestCase):
     def __init__(self, method_name: str = "runTest") -> None:
         super().__init__(method_name)
 
-    @patch("requests.get", side_effect=mocked_request_get_404)
+    @patch("requests.get", side_effect=mocked_request_404)
     def test_government_borrowing_rate_status_code_not_200(self, mock_get) -> None:
         """Assert government_borrowing_rate raises error if status not 200."""
         with self.assertRaises(ConnectionError):
@@ -55,13 +61,13 @@ class TestDownload(unittest.TestCase):
         )
         self.assertEqual(test_df.iloc[2, 2], np.float64(2.16))
 
-    @patch("requests.post", side_effect=mocked_request_get_404)
+    @patch("requests.post", side_effect=mocked_request_404)
     def test_consumer_price_index_status_code_not_200(self, mock_post) -> None:
         """Assert consumer_price_index raises error if status not 200."""
         with self.assertRaises(ConnectionError):
             download.consumer_price_index()
 
-    @patch("requests.post", side_effect=mocked_requests_get_200_cpi)
+    @patch("requests.post", side_effect=mocked_requests_post_200_cpi)
     def test_consumer_price_index_status_code_200(self, mock_post) -> None:
         """Assert consumer_price_index imports data correctly."""
         test_df = download.consumer_price_index()
@@ -69,6 +75,24 @@ class TestDownload(unittest.TestCase):
             list(
                 test_df.columns.values,
             ),
-            ["Datum", "Konsumentprisindex"],
+            ["date", "consumer_price_index"],
         )
         self.assertEqual(test_df.iloc[1, 1], np.float64(96.8))
+
+    @patch("requests.get", side_effect=mocked_request_404)
+    def test_policy_rate_status_code_not_200(self, mock_get) -> None:
+        """Assert consumer_price_index raises error if status not 200."""
+        with self.assertRaises(ConnectionError):
+            download.policy_rate()
+
+    @patch("requests.get", side_effect=mocked_requests_get_200_pr)
+    def test_policy_rate_status_code_200(self, mock_get) -> None:
+        """Assert policy_rate imports data correctly."""
+        test_df = download.policy_rate()
+        self.assertEqual(
+            list(
+                test_df.columns.values,
+            ),
+            ["date", "policy_rate"],
+        )
+        self.assertEqual(test_df.iloc[2, 1], np.float64(6.95))
